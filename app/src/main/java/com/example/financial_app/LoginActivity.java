469d349d2ebc91d90c88cb9e57ba492e640fb301
Yakin17 <ishimweyakin8@gmail.com>
2025-05-17 12:59:39 +0300

    Correction espace url

--- app/src/main/java/com/example/financial_app/LoginActivity.java
+++ app/src/main/java/com/example/financial_app/LoginActivity.java
@@ -55,6 +55,9 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String newUrl = serverUrlEditText.getText().toString().trim();
+                // Supprimer tous les espaces dans l'URL pour éviter les erreurs de connexion
+                newUrl = newUrl.replaceAll("\\s+", "");
+
                 if (!newUrl.isEmpty() && newUrl.startsWith("http")) {
                     // Ensure URL ends with /api/
                     if (!newUrl.endsWith("/api/")) {
